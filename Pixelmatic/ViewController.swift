699ffc130c0ef8af30d8bd30decec86d15a4172b
Ilia <iStukalov96@yandex.ru>
2018-02-15 13:01:29 +0300

    Initial Commit

---
+++ Pixelmatic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pixelmatic
+//
+//  Created by Ilia on 15/02/2018.
+//  Copyright © 2018 Ilia Stukalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
